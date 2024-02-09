f8680c29a9cc8c1eccd3cabdd4cf194754b88ba3
ArthurZhem <arthyme@inbox.ru>
2024-02-09 23:48:50 +0500

    Сделал часть второй домашней работы

---
+++ hw-2.js
@@ -0,0 +1,43 @@
+let a, b;
+
+// Задание 1
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+const iphone = 2007;
+alert(iphone);
+
+// Задание 3
+const creator = "Brendan Eich";
+alert(creator);
+
+// Задание 4
+a = 10;
+b = 2;
+
+alert(sum);
+alert(difference);
+alert(product);
+alert(quolitat);
+
+// Задание 5
+a = 2 ** 5;
+alert(a);
+
+// Задание 6
+a = 9;
+b = 2;
+alert(quolitat);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
